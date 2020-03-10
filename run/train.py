2ec0cd7685ad436d27c0bc0b22177fb1f97a1239
KrisHan999 <khan7@uci.edu>
2020-03-09 20:19:34 -0700

    refine train

--- run/train.py
+++ run/train.py
@@ -27,26 +27,34 @@ def train(model_path=None):
         model_config = config_param.model.TD_SV_model
         train_config = config_param.train.TD_SV_train
 
-    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # load dataset
     train_set = SpeechDataset()
     train_loader = DataLoader(train_set, batch_size=train_config.N, shuffle=False, drop_last=True)
 
+    # construct midel
     speech_embedder = SpeechEmbedder().to(device)
     ge2e_loss = GE2ELoss().to(device)
 
+    # whether restore previous saved model
     if train_config.restore:
         model_load = torch.load(model_path)
         speech_embedder.load_state_dict(model_load['speech_embedder'])
         ge2e_loss.load_state_dict(model_load['ge2e_loss'])
 
+    # set optimizer. For different component, set different gradient scale
     optimizer = torch.optim.SGD([
         {'params': speech_embedder.LSTM.parameters(), 'lr': train_config.lr*0.5},
         {'params': speech_embedder.projection.parameters(), 'lr': train_config.lr},
         {'params': ge2e_loss.parameters(), 'lr': train_config.lr*0.01}
     ])
 
+    # create file directory for saving training results.
     os.makedirs(train_config.checkpoint_dir, exist_ok=True)
+    os.makedirs(os.path.dirname(train_config.log_file), exist_ok=True)
 
+    # start train
     speech_embedder.train()
     iteration = 0
     for e in range(train_config.epochs):
@@ -55,25 +63,26 @@ def train(model_path=None):
 
             # print("epoch: {}, batch: {}".format(i, batch_id))
             iteration += 1
+            optimizer.zero_grad()
 
             batch = batch.to(device)
             N, M, frames, nmels = batch.shape
             batch = batch.reshape(N*M, frames, nmels)
 
+            # shuffle the training batch
             perm = random.sample(range(0, N*M), N*M)
             unperm = list(perm)
             for i, j in enumerate(perm):
                 unperm[j] = i
-
             batch_shuffle = batch[perm]
-
-            optimizer.zero_grad()
             embedding_shuffle = speech_embedder(batch_shuffle)
+            # un-shuffle batch for similarity
             embedding = embedding_shuffle[unperm]
 
             loss = ge2e_loss(embedding)
             loss.backward()
 
+            # avoid gradient exploding
             torch.nn.utils.clip_grad_norm_(speech_embedder.parameters(), 3.0)
             torch.nn.utils.clip_grad_norm_(ge2e_loss.parameters(), 3.0)
 
@@ -81,6 +90,7 @@ def train(model_path=None):
 
             total_loss += loss
 
+            # print training loss every interval batches
             if (batch_id + 1) % train_config.log_intervals == 0:
                 mesg = "{0}\tEpoch:{1}[{2}/{3}],Iteration:{4}\tLoss:{5:.4f}\tTLoss:{6:.4f}\t\n".format(time.ctime(),
                                                                                                        e + 1,
@@ -91,10 +101,10 @@ def train(model_path=None):
                                                                                                        total_loss / (
                                                                                                                    batch_id + 1))
                 print(mesg)
-                os.makedirs(os.path.dirname(train_config.log_file), exist_ok=True)
                 with open(train_config.log_file, 'a') as f:
                     f.write(mesg)
 
+        # save checkpoint every interval epochs
         if (i+1) % train_config.log_intervals == 0:
             speech_embedder.eval().cpu()
             ge2e_loss.eval().cpu()
@@ -104,6 +114,7 @@ def train(model_path=None):
             speech_embedder.to(device).train()
             ge2e_loss.to(device).train()
 
+    # save final model
     speech_embedder.eval().cpu()
     ge2e_loss.eval().cpu()
     ckpt_model_filename = "final_epoch_" + str(e + 1) + "_batch_id_" + str(batch_id + 1) + ".model"
